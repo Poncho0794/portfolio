9abadf0fbfffe3f70720aaacef5084b0ebd2efaa
Alfonso Banda <abanda@apexsystems.com>
2022-09-16 15:25:41 -0500

    Initial files and add navbar

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,9 @@
+import icon from '../../assets/portfolio-icon.svg'
+import './styles.css'
+function Navbar() {
+    return (<div className='navbar-container'>
+        <img src={icon} />
+
+    </div>)
+}
+export default Navbar;
