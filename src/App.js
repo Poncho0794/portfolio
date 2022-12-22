34604568cfe5b54d4922cfc9444648deb8ee77cb
Alfonso Banda <abanda@apexsystems.com>
2022-12-21 21:55:58 -0600

    Complete Work Experience

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Navbar from './components/Navbar';
 import Home from './sections/Home'
 import Slogan from './sections/Slogan';
+import WorkExperience from './sections/WorkExperience';
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Home />
       <Slogan />
+      <WorkExperience />
     </div>
   );
 }
---
+++ src/sections/WorkExperience/index.jsx
@@ -0,0 +1,40 @@
+import "./styles.css";
+import webAppPhoto from "../../assets/web_app.png";
+import teaching from "../../assets/teaching.png";
+import fullstackDev from "../../assets/fullstack_develop.png";
+
+const WorkExperience = () => {
+  return (
+    <div className="work-experience-container">
+      <h1>Work Experience</h1>
+      <div className="work-experience-row">
+        <div className="experience-item">
+          <img src={webAppPhoto} alt="Web application"/>
+          <h2>Web Applications</h2>
+          <p>
+            Creating professional web application from scratch, designing and
+            structuring from the client requirements
+          </p>
+        </div>
+        <div className="experience-item">
+          <img src={fullstackDev}  alt="Web application"/>
+          <h2>Fullstack Development</h2>
+          <p>
+            Working in both sides of an application, implemeting my own APIs,
+            connect differents serivces like AWS or MongoDB Atlas
+          </p>
+        </div>
+        <div className="experience-item">
+          <img src={teaching}  alt="Web application"/>
+          <h2>Teaching</h2>
+          <p>
+            Teaching web development on differents courses and creating my own
+            online courses
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default WorkExperience;
